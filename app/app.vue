0e25f169d31d0ae68b1599d7a4e9998de70c7664
rakamiracle <rakasabri6@gmail.com>
2025-08-15 07:33:09 +0700

    two

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="min-h-screen flex items-center justify-center bg-blue-500">
+    <h1 class="text-4xl font-bold text-white">
+      Dynamic CMS Web
+    </h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/image', '@nuxt/ui']
+  css: ['@/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
